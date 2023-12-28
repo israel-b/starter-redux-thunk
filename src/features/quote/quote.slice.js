5337e60e5fc5cbf8974cc914544f32bb5f9755f2
israel-b <briggs.israel@gmail.com>
2023-12-28 13:35:40 -0800

    Completed exercises

--- src/features/quote/quote.slice.js
+++ src/features/quote/quote.slice.js
@@ -1,6 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'; // Add createAsyncThunk to the list of imports
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'; // Add createAsyncThunk to the list of imports
 
 // Add fetchQuote() here
+export const fetchQuote = createAsyncThunk(
+  'quote/fetchQuote',
+  async (arg, thunkAPI) => {
+    const response = await fetch('http://localhost:3004/api/quote');
+    const { data } = await response.json();
+    return data;
+  }
+)
 
 const initialState = {
   quote: '',
@@ -12,6 +20,21 @@ const options = {
   name: 'quote',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchQuote.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [fetchQuote.fulfilled]: (state, { payload: { quote, author } }) => {
+      state.quote = { quote, author };
+      state.loading = false;
+      state.error = false;
+    },
+    [fetchQuote.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 };
 
 const quoteSlice = createSlice(options);
@@ -21,3 +44,4 @@ export default quoteSlice.reducer;
 export const selectQuote = (state) => state.quote.quote;
 export const selectLoading = (state) => state.quote.loading;
 export const selectError = (state) => state.quote.error;
+
